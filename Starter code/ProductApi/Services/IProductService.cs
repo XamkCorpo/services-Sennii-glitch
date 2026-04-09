34759ede96667c211df23a48f05774a84afd4810
Omistaja <Omistaja@DESKTOP-T4KILA5>
2026-04-09 14:56:09 +0300

    vaihe 2

---
+++ Starter code/ProductApi/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using ProductApi.Models.Dtos;
+
+namespace ProductApi.Services;
+
+public interface IProductService
+{
+    Task<List<ProductResponse>> GetAllAsync();
+    Task<ProductResponse?> GetByIdAsync(int id);
+    Task<ProductResponse> CreateAsync(CreateProductRequest request);
+    Task<ProductResponse?> UpdateAsync(int id, UpdateProductRequest request);
+    Task<bool> DeleteAsync(int id);
+}
